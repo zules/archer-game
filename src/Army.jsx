75678767ee55cbf5a7bac0a43045f861de0e79ff
zules <5903782+zules@users.noreply.github.com>
2026-01-15 22:25:58 -0600

    feat: add unit names to Army and Unit components

--- src/Army.jsx
+++ src/Army.jsx
@@ -2,10 +2,17 @@ import Unit from './Unit.jsx';
 
 export default function Army(props) {
     const unitNums = Array.from({ length: 9 }, (_, i) => i + 1);
+
+    const unitNames = props.className === "enemy army"
+        //enemy names
+        ? ["Ernie", "Seth", "Luna", "Zane", "Mira", "Jax", "Nia", "Kade", "Vera"]
+        //user names
+        : ["Michael", "Burt", "Cid", "Dax", "Emi", "Finn", "Gia", "Hope", "Ike"];
+
     return (
         <div className={props.className}>
         {unitNums.map((id) => (
-            <Unit key={id} id={id} />
+            <Unit key={id} id={id} name={unitNames[id - 1]} />
         ))}
         </div>
     )
--- src/Unit.jsx
+++ src/Unit.jsx
@@ -2,6 +2,7 @@ export default function Unit(props) {
     return (
         <div className="unit" id={props.id}>
             <p>Unit: {props.id}</p>
+            <p>{props.name}</p>
         </div>
     );
 }
