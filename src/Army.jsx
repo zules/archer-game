1aa52e879602d2ce7f8d4491639860db202afe43
zules <5903782+zules@users.noreply.github.com>
2026-01-15 21:58:24 -0600

    feat: populate Unit components with one specified number

--- src/Army.jsx
+++ src/Army.jsx
@@ -1,17 +1,12 @@
 import Unit from './Unit.jsx';
 
 export default function Army(props) {
+    const unitNums = Array.from({ length: 9 }, (_, i) => i + 1);
     return (
         <div className={props.className}>
-            <Unit id="1" />
-            <Unit id="2" />
-            <Unit id="3" />
-            <Unit id="4" />
-            <Unit id="5" />
-            <Unit id="6" />
-            <Unit id="7" />
-            <Unit id="8" />
-            <Unit id="9" />
+        {unitNums.map((id) => (
+            <Unit key={id} id={id} />
+        ))}
         </div>
     )
 }
