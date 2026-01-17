ee0ec5cf960cad97376c36f7dd8d5f75454284ca
zules <5903782+zules@users.noreply.github.com>
2026-01-17 14:02:50 -0600

    add game unit data

---
+++ src/uniques.js
@@ -0,0 +1,14 @@
+export const UNIQUES = [
+  ["01", {
+    name: "Pepper Archer",
+    atk: 7, health: 4, speed: 9,
+    flavor: "A baddie", }],
+  ["02", {
+    name: "Cutie Pie",
+    atk: 1, health: 3, speed: 2,
+    flavor: "A cutie", }],
+  ["03", {
+    name: "Star Shooter",
+    atk: 8, health: 2, speed: 4,
+    flavor: "A star", }],
+];
