4c927b086cf55e5072ac3b40472d85541adfd4c3
zules <5903782+zules@users.noreply.github.com>
2026-01-17 14:47:42 -0600

    display stats for each unit on board

--- src/Unit.jsx
+++ src/Unit.jsx
@@ -6,6 +6,11 @@ export default function Unit(props) {
     return (
         <div className={`unit ${props.id}`}>
             <p>{unitLookup.get(props.id).name}</p>
+            <div className="stats">
+                <p>ATK: {unitLookup.get(props.id).atk}</p>
+                <p>HP: {unitLookup.get(props.id).health}</p>
+                <p>SPD: {unitLookup.get(props.id).speed}</p>
+            </div>
         </div>
     );
 }
