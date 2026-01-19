fa37b72a64dede4dfce64aa50aa1ced8d59aaec3
zules <5903782+zules@users.noreply.github.com>
2026-01-18 20:05:59 -0600

    reorganize army initialization

--- src/TheGame.jsx
+++ src/TheGame.jsx
@@ -11,32 +11,34 @@ function handleButtonClick() {
     alert('Button clicked!');
 }
 
+// Set initial armies
+const enemyArmyInitial = randomArmy();
+const userArmyInitial = randomArmy();
+
+// Initialize armies with unit data
+const initializeArmy = (armyVar, armyType) => {
+    return armyVar.map((id, index) => {
+        const unitData = UNIQUES.get(id);
+        return {
+            ///SPREAD operator
+            ...unitData,
+            instanceId: `${armyType}-${index+1}`,
+            currentHp: unitData.health,
+        }
+    })
+}
+
 
 export default function TheGame () {
+    
+    // Initialize state for both armies
+    const [enemyArmy, setEnemyArmy] = useState(() => 
+    initializeArmy(enemyArmyInitial, "enemy")
+    );
+    const [userArmy, setUserArmy] = useState(() => 
+    initializeArmy(userArmyInitial, "user")
+    );
 
-        // Set initial armies
-        const enemyArmyInitial = randomArmy();
-        const userArmyInitial = randomArmy();
-
-        // Initialize armies with unit data
-        const initializeArmy = (armyVar, armyType) => {
-            return armyVar.map((id, index) => {
-                const unitData = UNIQUES.get(id);
-                return {
-                    ///SPREAD operator
-                    ...unitData,
-                    instanceId: `${armyType}-${index+1}`,
-                    currentHp: unitData.health,
-                }
-            })
-        }
-        const [enemyArmy, setEnemyArmy] = useState(() => 
-            initializeArmy(enemyArmyInitial, "enemy")
-        );
-        const [userArmy, setUserArmy] = useState(() => 
-            initializeArmy(userArmyInitial, "user")
-        );
-        
         // End initialization and determine which unit attacks first
         
         return (
