73ab1bdbfa5b31adf862b933a23744d57158fea5
zules <5903782+zules@users.noreply.github.com>
2026-01-25 13:21:21 -0600

    remove start turn button if game is over

--- src/GameUI.jsx
+++ src/GameUI.jsx
@@ -1,4 +1,8 @@
 export default function GameUI({ onButtonClick, buttonText, isGameOver }) {
+    if (isGameOver) {
+        return <h2 className="game-over">Game Over!</h2>;
+    }
+
     return (
         <button disabled={isGameOver} className="start-turn" onClick={onButtonClick}>{buttonText}</button>
     );
