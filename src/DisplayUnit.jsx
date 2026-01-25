fceee62b815bb1484cdeec9fd560b7b9607c69d8
zules <5903782+zules@users.noreply.github.com>
2026-01-25 15:30:10 -0600

    style unit cards to show engagement and aliveness

--- src/DisplayUnit.jsx
+++ src/DisplayUnit.jsx
@@ -3,7 +3,7 @@ export default function DisplayUnit({unitData}) {
         const { name, atk, currentHp, hp, spd, clan, acc, gly } = unitData;
 
         return (
-        <div className="unit">
+        <div className={`unit ${unitData.engaged ? "engaged" : ""} ${currentHp <= 0 ? "dead" : ""}`}>
             <p>{name}</p>
             <div>
                 <div className="stats">
@@ -17,7 +17,6 @@ export default function DisplayUnit({unitData}) {
                 <div className="stats">
                     <p>GLY: {gly}</p>
                     <p>{clan}{clan != "Scarestare" ? "er" : "r"}</p>
-                    <p>Engaged? {unitData.engaged ? "Yes" : "No"}</p>
                 </div>
             </div>
         </div>
