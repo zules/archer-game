7cf0a7355006a9c8a5f78e29606a3133a3e678d6
zules <5903782+zules@users.noreply.github.com>
2026-01-31 17:25:36 -0600

    add arrow indicators to show which side attacks

--- src/Lane.jsx
+++ src/Lane.jsx
@@ -9,15 +9,26 @@ const [topLaneMsg, setTopLaneMsg] = useState("");
 const [midLaneMsg, setMidLaneMsg] = useState("");
 const [botLaneMsg, setBotLaneMsg] = useState("");
 
+const [topLaneArrows, setTopLaneArrows] = useState("");
+const [midLaneArrows, setMidLaneArrows] = useState("");
+const [botLaneArrows, setBotLaneArrows] = useState("");
+
+
 useEffect(() => {
     if (log.length === 0) {
-        setTopLaneMsg("");
-        setMidLaneMsg("");
-        setBotLaneMsg("");
         return;
     }
 
 const runBattlePlayback = (log) => {
+        if (log.length === 0) {
+        setTopLaneMsg("");
+        setMidLaneMsg("");
+        setBotLaneMsg("");
+        setTopLaneArrows("");
+        setMidLaneArrows("");
+        setBotLaneArrows("");
+        return;
+    }
 
     const [currentEvent, ...remainingEvents] = log;
     const {attacker, defender, attackPower} = currentEvent;
@@ -26,10 +37,24 @@ const runBattlePlayback = (log) => {
 
     const message = `${attacker} shot ${defender} for ${attackPower} dmg.`
 
+    let arrows;
+
+    switch (side) {
+        case 'user':
+            arrows = "← ←"
+            break;
+        case 'enemy':
+            arrows = "→ →"
+            break;
+    }
+
     switch (position) {
         case 1:
         case 4:
         case 7:
+            setTopLaneArrows(arrows);
+            setMidLaneArrows("");
+            setBotLaneArrows("");
             setTopLaneMsg(message);
             setMidLaneMsg("");
             setBotLaneMsg("");
@@ -37,6 +62,9 @@ const runBattlePlayback = (log) => {
         case 2:
         case 5:
         case 8:
+            setTopLaneArrows("");
+            setMidLaneArrows(arrows);
+            setBotLaneArrows("");
             setTopLaneMsg("");
             setMidLaneMsg(message);
             setBotLaneMsg("");
@@ -44,6 +72,9 @@ const runBattlePlayback = (log) => {
         case 3:
         case 6:
         case 9:
+            setTopLaneArrows("");
+            setMidLaneArrows("");
+            setBotLaneArrows(arrows);
             setTopLaneMsg("");
             setMidLaneMsg("");
             setBotLaneMsg(message);
@@ -62,12 +93,15 @@ runBattlePlayback(log);
     return (
         <div className="lanes">
                 <div key={1} className="lane">
+                    <p className="arrows">{topLaneArrows}</p>
                     <p>{topLaneMsg}</p>
                 </div>
                 <div key={2} className="lane">
+                    <p className="arrows">{midLaneArrows}</p>
                     <p>{midLaneMsg}</p>
                 </div>
                 <div key={3} className="lane">
+                    <p className="arrows">{botLaneArrows}</p>
                     <p>{botLaneMsg}</p>
                 </div>
         </div>
