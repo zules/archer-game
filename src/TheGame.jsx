4e5c25489aa778482a0c5533e9481876c893cf31
zules <5903782+zules@users.noreply.github.com>
2026-01-31 16:08:47 -0600

    clean up code and put game logic functions into separate file for import

--- src/TheGame.jsx
+++ src/TheGame.jsx
@@ -1,35 +1,14 @@
 import { useState } from 'react';
 
-import { UNIQUES } from './uniques';
-import { randomArmy } from './battleCalcs';
+import { randomArmy, initializeArmy, establishEngagedUnits, createAttackQueue, performAttacks } from './battleCalcs';
 
 import Battlefield from './Battlefield.jsx'
 import Lane from './Lane.jsx'
 import GameUI from './GameUI.jsx';
 
-// Row index references
-const row1 = [0,3,6];
-const row2 = [1,4,7];
-const row3 = [2,5,8];
-
-
 // Set initial armies
 const enemyArmyInitial = randomArmy();
 const userArmyInitial = randomArmy();
-
-// Initialize armies with unit data
-const initializeArmy = (armyVar, armyType) => {
-    return armyVar.map((id, index) => {
-        const unitData = UNIQUES.get(id);
-        return {
-            ///SPREAD operator
-            ...unitData,
-            instanceId: `${armyType}-${index+1}`,
-            currentHp: unitData.hp,
-            engaged: false,
-        }
-    })
-}
 
 export default function TheGame () {
     
@@ -74,202 +53,25 @@ export default function TheGame () {
         if (isGameOver) return alert("Error: The game is over.");
 
         // Determine which units are engaged to attack this turn
+        const readiedUserArmy = establishEngagedUnits(userArmy, userArmy, enemyArmy);
+        const readiedEnemyArmy = establishEngagedUnits(enemyArmy, userArmy, enemyArmy);
 
-        function establishEngagedUnits(army) {
-            let armyToReturn = [...army];
-            let oppositeArmy = army === userArmy ? [...enemyArmy] : [...userArmy];
-            
-            function goThroughRow(x,y,z,targetedArmy) {
-                const sumOfEnemyHp = [x,y,z].reduce((accumulator, currentIndex) => accumulator + oppositeArmy[currentIndex].currentHp, 0);
+        // Sort them
+        const sortedAttackers = createAttackQueue(readiedUserArmy, readiedEnemyArmy);
 
-                console.log(`Sum of opposite row HP for index ${x}${y}${z} is ${sumOfEnemyHp}.`)
-
-                return targetedArmy.map((unit, index) => {
-                        if (unit.currentHp <= 0) {
-                            console.log(`Unit at index ${index} has less than zero health. Setting engaged to false.`)
-                            return {...unit, engaged: false};
-                        }
-
-                        if (sumOfEnemyHp <= 0 && (index === x || index === y || index === z)) {
-                            console.log(`Unit at index ${index} has defeated all enemies. Setting engaged to false.`)
-                            return {...unit, engaged: false};
-                        }
-
-                        else {
-                            if (index == x && unit.currentHp > 0) {
-                                console.log(`Unit at index ${index} is a front line unit ready to attack.`)
-                                return {...unit, engaged: true};
-                            }
-                            else if (index == y && targetedArmy[x].currentHp <= 0 && unit.currentHp > 0) {
-                                console.log(`Unit at index ${index} is a middle line unit ready to attack.`)
-                                return {...unit, engaged: true};
-                            }
-                            else if (index == z && targetedArmy[x].currentHp <= 0 && targetedArmy[y].currentHp <= 0 && unit.currentHp > 0) {
-                                console.log(`Unit at index ${index} is a back line unit ready to attack.`)
-                                return {...unit, engaged: true};
-                            }
-                            else {
-                                return unit;
-                            }
-                        }
-                    })
-                }
-            
-            armyToReturn = goThroughRow(...row1, armyToReturn);
-            armyToReturn = goThroughRow(...row2, armyToReturn);
-            armyToReturn = goThroughRow(...row3, armyToReturn);
-
-            return armyToReturn;
-            }
-
-        const readiedUserArmy = establishEngagedUnits(userArmy);
-        const readiedEnemyArmy = establishEngagedUnits(enemyArmy);
-
-        console.log("Readying user army as:")
-        console.table(readiedUserArmy);
-        console.log("Readying enemy army as:")
-        console.table(readiedEnemyArmy);
-
-        // Sort attackers into a queue that accounts for speed
-
-        const bothArmies = [...readiedUserArmy, ...readiedEnemyArmy];
-
-        const sortedAttackers = bothArmies
-            .filter(unit => unit.engaged)
-            .map(unit => ({...unit, tieBreakRoll: Math.random() }))
-            .sort((a, b) => {
-                if (b.spd !== a.spd) {
-                    return b.spd - a.spd;
-                }
-                return b.tieBreakRoll - a.tieBreakRoll;
-                })
-            .map(unit => unit.instanceId);
-
-        console.log(`Speed queue is ${sortedAttackers}.`)
-        console.log(`Sorted attackers type detected as:`)
-        console.log(typeof sortedAttackers)
-        console.table(sortedAttackers);
-        console.log(`Units in sorted attackers amounts to ${sortedAttackers.length}`)
+        // Check if game is over
         if (sortedAttackers.length <= 0) {
-            
-            console.log(`Detecting no speed queue. Changing GAMEOVER status to true.`)
             setIsGameOver(true);
         }
 
-        // Get ready to perform attacks
+        // Perform attacks
+        const { userArmyAfterAttacks, enemyArmyAfterAttacks } = performAttacks(sortedAttackers, readiedUserArmy, readiedEnemyArmy);
 
-        let userArmyAfterAttacks = [...readiedUserArmy];
-        let enemyArmyAfterAttacks = [...readiedEnemyArmy];
-
-        function performAttacks() {
-
-            sortedAttackers.forEach(unit => {
-
-                console.log(`Running attack for ${unit} from sortedAttackers`)
-                // Determine which unit is being attacked
-                let target;
-                let validTargets;
-                const [side, position] = unit.split("-");
-
-                const oppositeSide = side === "user" ? "enemy" : "user";
-
-                if (position == 1 || position == 4 || position == 7) {
-                    validTargets = [`${oppositeSide}-1`, `${oppositeSide}-4`, `${oppositeSide}-7`]
-                }
-                else if (position == 2 || position == 5 || position == 8) {
-                    validTargets = [`${oppositeSide}-2`, `${oppositeSide}-5`, `${oppositeSide}-8`]
-                }
-                else if (position == 3 || position == 6 || position == 9) {
-                    validTargets = [`${oppositeSide}-3`, `${oppositeSide}-6`, `${oppositeSide}-9`]
-                }
-                else {
-                    console.log("Error in performAttacks validTargets calculation");
-                }
-                
-                console.log(`This unit might be attacking ${validTargets}`)
-
-                const defendingArmyStats = side === "user" ? enemyArmyAfterAttacks : userArmyAfterAttacks;
-                const attackingArmyStats = side === "user" ? userArmyAfterAttacks : enemyArmyAfterAttacks;
-
-                target = validTargets.find(p => sortedAttackers.includes(p));
-
-                console.log(`Trying to find target... target is ${target}`);
-
-                let isTargetAlive;
-
-                if (target === undefined) {
-                    console.log(`Wait, target not found. Setting aliveness to false.`)
-                    isTargetAlive = false;
-                }
-                else {
-                    console.log(`Checking if target is still alive.`)
-                    isTargetAlive = defendingArmyStats.find(i => i.instanceId === target).currentHp > 0 ? true : false;
-                    console.log(`Are they? ${isTargetAlive}`)
-                }
-
-                let isAttackerStillAlive = attackingArmyStats.find(i => i.instanceId === unit).currentHp > 0 ? true : false;
-
-
-                console.log(`In summary: the next target is ${target} and isAlive is ${isTargetAlive}`);
-
-                // if (validTargets.find(p => sortedAttackers.includes(p))) {
-                //     if (defendingArmyStats.find(i => i.instanceId === ))
-                //     target = validTargets.find(p => sortedAttackers.includes(p));
-                // }
-
-                function makeAttack (attackerId, attackedId) {
-
-                    // NOTE TO SELF clean up this section by using const defendingArmyStats and const attackingArmyStats
-                    
-                    if (side === "user") {
-                        const attackPower = userArmyAfterAttacks.find(power => power.instanceId === attackerId).atk;
-                        enemyArmyAfterAttacks = enemyArmyAfterAttacks.map( u => {
-                            if (u.instanceId === attackedId) {
-                                let newHp = u.currentHp - attackPower;
-                                newHp = Math.max(0, newHp);
-                                return {...u, currentHp: newHp}
-                            }
-                            return u;
-                        })
-                    console.log(`${attackedId} is attacked by ${attackerId} with a ${attackPower} HP attack!`)
-                    }
-
-                    else if (side === "enemy") {
-                        const attackPower = enemyArmyAfterAttacks.find(power => power.instanceId === attackerId).atk;
-                        userArmyAfterAttacks = userArmyAfterAttacks.map( u => {
-                            if (u.instanceId === attackedId) {
-                                let newHp = u.currentHp - attackPower;
-                                newHp = Math.max(0, newHp);
-                                return {...u, currentHp: newHp}
-                            }
-                            return u;
-                        })
-                    console.log(`${attackedId} is attacked by ${attackerId} with a ${attackPower} HP attack!`)
-                    }
-
-                }
-
-                if (isTargetAlive && isAttackerStillAlive) {
-                makeAttack(unit,target);
-                }
-
-            }
-            )
-
-        }
-
-        performAttacks();
-
-        console.log("Turn ends.")
-
-        // REMINDER TO PUT FINAL ARMY STATUS INTO STATE HERE WITH MOST RECENT VARS.
-
+        // Update state
         setUserArmy(userArmyAfterAttacks);
         setEnemyArmy(enemyArmyAfterAttacks);
 
     }
-
-
         
         return (
             <>
--- src/battleCalcs.js
+++ src/battleCalcs.js
@@ -1,7 +1,12 @@
-import { UNIQUES_ARRAY } from './uniques';
+import { UNIQUES, UNIQUES_ARRAY } from './uniques';
 
 // An array of all unique unit numbers
 const unitNumbers = UNIQUES_ARRAY.map(([num, obj]) => num);
+
+// Row index references
+const row1 = [0,3,6];
+const row2 = [1,4,7];
+const row3 = [2,5,8];
 
 export const randomArmy = () => {
     const randomEntries = [];
@@ -11,3 +16,173 @@ export const randomArmy = () => {
     }
     return randomEntries;
 };
+
+// Initialize armies with unit data
+export const initializeArmy = (armyVar, armyType) => {
+    return armyVar.map((id, index) => {
+        const unitData = UNIQUES.get(id);
+        return {
+            ///SPREAD operator
+            ...unitData,
+            instanceId: `${armyType}-${index+1}`,
+            currentHp: unitData.hp,
+            engaged: false,
+        }
+    })
+}
+
+// Determine which units are engaged to attack this round.
+export function establishEngagedUnits(army, userArmy, enemyArmy) {
+    let armyToReturn = [...army];
+    let oppositeArmy = army === userArmy ? [...enemyArmy] : [...userArmy];
+    
+    function goThroughRow(x,y,z,targetedArmy) {
+        const sumOfEnemyHp = [x,y,z].reduce((accumulator, currentIndex) => accumulator + oppositeArmy[currentIndex].currentHp, 0);
+
+        console.log(`Sum of opposite row HP for index ${x}${y}${z} is ${sumOfEnemyHp}.`)
+
+        return targetedArmy.map((unit, index) => {
+                if (unit.currentHp <= 0) {
+                    console.log(`Unit at index ${index} has less than zero health. Setting engaged to false.`)
+                    return {...unit, engaged: false};
+                }
+
+                if (sumOfEnemyHp <= 0 && (index === x || index === y || index === z)) {
+                    console.log(`Unit at index ${index} has defeated all enemies. Setting engaged to false.`)
+                    return {...unit, engaged: false};
+                }
+
+                else {
+                    if (index == x && unit.currentHp > 0) {
+                        console.log(`Unit at index ${index} is a front line unit ready to attack.`)
+                        return {...unit, engaged: true};
+                    }
+                    else if (index == y && targetedArmy[x].currentHp <= 0 && unit.currentHp > 0) {
+                        console.log(`Unit at index ${index} is a middle line unit ready to attack.`)
+                        return {...unit, engaged: true};
+                    }
+                    else if (index == z && targetedArmy[x].currentHp <= 0 && targetedArmy[y].currentHp <= 0 && unit.currentHp > 0) {
+                        console.log(`Unit at index ${index} is a back line unit ready to attack.`)
+                        return {...unit, engaged: true};
+                    }
+                    else {
+                        return unit;
+                    }
+                }
+            })
+        }
+    
+    armyToReturn = goThroughRow(...row1, armyToReturn);
+    armyToReturn = goThroughRow(...row2, armyToReturn);
+    armyToReturn = goThroughRow(...row3, armyToReturn);
+
+    return armyToReturn;
+    }
+
+export function createAttackQueue (readiedUserArmy, readiedEnemyArmy) {
+        // Sort attackers into a queue that accounts for speed
+        const bothArmies = [...readiedUserArmy, ...readiedEnemyArmy];
+
+        const sortedAttackers = bothArmies
+            .filter(unit => unit.engaged)
+            .map(unit => ({...unit, tieBreakRoll: Math.random() }))
+            .sort((a, b) => {
+                if (b.spd !== a.spd) {
+                    return b.spd - a.spd;
+                }
+                return b.tieBreakRoll - a.tieBreakRoll;
+                })
+            .map(unit => unit.instanceId);
+
+        return sortedAttackers;
+
+        }
+
+
+// Perform the attacks
+export function performAttacks(sortedAttackers, userArmyForAttacks, enemyArmyForAttacks) {
+
+    let userArmyDuringAttacks = [...userArmyForAttacks];
+    let enemyArmyDuringAttacks = [...enemyArmyForAttacks];
+
+            sortedAttackers.forEach(unit => {
+
+                // Determine which unit is being attacked
+                let target;
+                let validTargets;
+                const [side, position] = unit.split("-");
+
+                const oppositeSide = side === "user" ? "enemy" : "user";
+
+                if (position % 3 === 1) {
+                    validTargets = [`${oppositeSide}-1`, `${oppositeSide}-4`, `${oppositeSide}-7`]
+                }
+                else if (position % 3 === 2) {
+                    validTargets = [`${oppositeSide}-2`, `${oppositeSide}-5`, `${oppositeSide}-8`]
+                }
+                else if (position % 3 === 0) {
+                    validTargets = [`${oppositeSide}-3`, `${oppositeSide}-6`, `${oppositeSide}-9`]
+                }
+                else {
+                    throw new Error(`Invalid position in performAttacks: "${position}". Expected a value between 1 and 9.`);
+                }
+                
+                console.log(`This unit might be attacking ${validTargets}`)
+
+                const defendingArmyStats = side === "user" ? enemyArmyDuringAttacks : userArmyDuringAttacks;
+                const attackingArmyStats = side === "user" ? userArmyDuringAttacks : enemyArmyDuringAttacks;
+
+                target = validTargets.find(p => sortedAttackers.includes(p));
+
+                let isTargetAlive;
+
+                if (target === undefined) {
+                    isTargetAlive = false;
+                }
+                else {
+                    isTargetAlive = defendingArmyStats.find(i => i.instanceId === target).currentHp > 0 ? true : false;
+                }
+
+                let isAttackerStillAlive = attackingArmyStats.find(i => i.instanceId === unit).currentHp > 0 ? true : false;
+
+
+                function makeAttack (attackerId, attackedId) {
+
+                    const attackPower = attackingArmyStats.find(power => power.instanceId === attackerId).atk;
+
+                    if (side === "user") {
+                        enemyArmyDuringAttacks = enemyArmyDuringAttacks.map( u => {
+                            if (u.instanceId === attackedId) {
+                                let newHp = u.currentHp - attackPower;
+                                newHp = Math.max(0, newHp);
+                                return {...u, currentHp: newHp}
+                            }
+                            return u;
+                        })
+                    }
+
+                    else if (side === "enemy") {
+                        userArmyDuringAttacks = userArmyDuringAttacks.map( u => {
+                            if (u.instanceId === attackedId) {
+                                let newHp = u.currentHp - attackPower;
+                                newHp = Math.max(0, newHp);
+                                return {...u, currentHp: newHp}
+                            }
+                            return u;
+                        })
+                    }
+
+                }
+
+                if (isTargetAlive && isAttackerStillAlive) {
+                makeAttack(unit,target);
+                }
+
+            }
+            )
+
+            return {
+                userArmyAfterAttacks: userArmyDuringAttacks,
+                enemyArmyAfterAttacks: enemyArmyDuringAttacks,
+            }
+        }
