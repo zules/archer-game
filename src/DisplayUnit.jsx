056e3afc4940c2814e8c8e2562028285354355d6
zules <5903782+zules@users.noreply.github.com>
2026-02-09 15:45:51 -0600

    add abilities into unit data and UI

--- src/DisplayUnit.jsx
+++ src/DisplayUnit.jsx
@@ -2,11 +2,25 @@ import HealthBar from './HealthBar'
 
 export default function DisplayUnit({unitData}) {
 
-        const { name, atk, currentHp, hp, spd, clan, acc, gly } = unitData;
+        const { name, atk, currentHp, hp, spd, clan, acc, gly, abil } = unitData;
+
+        const ability = Object.values(abil).flat();
+
+        if (ability.length > 1) {
+            throw new Error (`Unit ${name} unexpectedly has more than 1 ability.`)
+        }
+
+        if (ability[0].amount != undefined && ability[0].amount < 1) {
+            ability[0].amount = ability[0].amount * 100;
+            ability[0].amount = `${ability[0].amount}%`;
+        }
 
         return (
         <div className={`unit ${unitData.engaged ? "engaged" : ""} ${currentHp <= 0 ? "dead" : ""} ${clan.toLowerCase()}`}>
             <p>{name}</p>
+            <div className="stats">
+                {ability[0] && <p>{ability[0].effect} ({ability[0].amount})</p>}
+            </div>
             <div>
                 <div className="stats">
                     <HealthBar currentHp={currentHp} hp={hp} />
--- src/uniques.js
+++ src/uniques.js
@@ -1,3 +1,5 @@
+import HealthBar from "./HealthBar";
+
 export const CLANS_STRONGEST_FIRST = [
   "Scarestare",
   "Secretkeep",
@@ -13,36 +15,101 @@ export const UNIQUES_ARRAY = [
     name: "Stern Elder",
     atk: 4, hp: 20, spd: 2, acc: 100, gly: 3,
     clan: "Scarestare",
+    abil: {
+      onEveryEngage: [
+        {
+          effect: "scare",
+          amount: 3,
+        }
+      ],
+      forAttack: [],
+      onGetKill: [],
+    },
     flavor: "Rumor is she knows everyone's birthday, even if you've never met her.", }],
   ["02", {
     name: "Absent Elder",
     atk: 7, hp: 1, spd: 8, acc: 90, gly: 3,
     clan: "Secretkeep",
+    abil: {
+      onEveryEngage: [],
+      forAttack: [
+        {
+          effect: "piercing",
+          amount: 3,
+        }
+      ],
+      onGetKill: [],
+    },
     flavor: "She lives on some cliff somewhere. Her eyes look haunted.", }],
   ["03", {
     name: "Respected Elder",
     atk: 5, hp: 8, spd: 9, acc: 90, gly: 3,
     clan: "Formstorm",
+    abil: {
+      onEveryEngage: [
+        {
+          effect: "blinding",
+          amount: .2,
+        }
+      ],
+      forAttack: [],
+      onGetKill: [],
+    },
     flavor: "She looks like she's all muscle, but she's smart as well.", }],
   ["04", {
     name: "Lighthouse Elder",
     atk: 7, hp: 8, spd: 3, acc: 85, gly: 3,
     clan: "Watercross",
+    abil: {
+      onEveryEngage: [
+        {
+          effect: "beloved",
+          amount: 1,
+        }
+      ],
+      forAttack: [],
+      onGetKill: [],
+    },
     flavor: "She's trying to reform the common currency.", }],
   ["05", {
     name: "Chanting Elder",
     atk: 3, hp: 7, spd: 4, acc: 50, gly: 3,
     clan: "Beatleap",
+    abil: {
+      onEveryEngage: [],
+      forAttack: [
+        {
+          effect: "heal",
+          amount: 4,
+        }
+      ],
+      onGetKill: [],
+    },
     flavor: "She's always trying to get people to join her side.", }],
   ["06", {
     name: "Inspirational Elder",
     atk: 2, hp: 17, spd: 7, acc: 90, gly: 3,
     clan: "Skymind",
+    abil: {
+      onEveryEngage: [],
+      forAttack: [],
+      onGetKill: [],
+    },
     flavor: "Her authorial debut has been delayed 15 years now.", }],
   ["07", {
     name: "Eldest Elder",
     atk: 12, hp: 10, spd: 2, acc: 10, gly: 3,
     clan: "Fossilcall",
+    abil: {
+      onEveryEngage: [],
+      forAttack: [],
+      onGetKill: [
+        {
+          effect: "rampaging",
+          amount: 20,
+        }
+      ],
+    },
     flavor: "She was born with three arms, and she's still mad they cut one off.", }],
 ];
 
