ac8a2e5cab958de3eb1d110e4d480689ca24d1c1
zules <5903782+zules@users.noreply.github.com>
2026-03-14 17:32:19 -0500

    fix game breaking if local storage does not yet exist

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import TheGame from './TheGame.jsx'
 import NewGame from './NewGame.jsx'
 import Home from './Home.jsx'
 import Onboarding from './Onboarding.jsx'
+import { useLocalStorage } from "@uidotdev/usehooks"
+import { Navigate } from 'react-router-dom'
 
 
 function App() {
+  const [savedClan] = useLocalStorage("savedClan", null);
 
   return (
-      <Routes>
-        <Route path="/newgame" element={<NewGame />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<TheGame />} />
-      </Routes>
+    <Routes>
+      <Route path="/newgame" element={<NewGame />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+      <Route path="/" element={savedClan ? <Home /> : <Navigate to="/newgame" />} />
+      <Route path="/game" element={savedClan ? <TheGame /> : <Navigate to="/newgame" />} />
+    </Routes>
   )
 }
 
